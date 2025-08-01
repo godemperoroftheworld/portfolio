f2c9a1f85ef4d105f812728aa00c3cd442b69b40
Tenzin Pelletier <t2pellet@uwaterloo.ca>
2025-08-01 08:54:00 -0400

    feat: start populating pages.yml

--- astro.config.ts
+++ astro.config.ts
@@ -2,10 +2,13 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
+import vue from "@astrojs/vue";
+import svgLoader from "vite-svg-loader";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap()],
+  integrations: [sitemap(), vue({ devtools: true })],
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss(), svgLoader()],
   },
 });
