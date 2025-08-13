37dbe2b76d9be438fc296a6af6a2d5e87604ccf7
Tenzin Pelletier <t2pellet@uwaterloo.ca>
2025-08-13 01:40:52 -0400

    feat: active header link

--- src/components/client/HeaderLink.vue
+++ src/components/client/HeaderLink.vue
@@ -1,16 +1,42 @@
 <script setup lang="ts">
 import scrollToElement from "@/utils/scroll.ts";
+import { computedWithControl, isClient, useWindowScroll } from "@vueuse/core";
+import { onMounted, ref } from "vue";
+
+const TOP_THRESHOLD = 100;
 
 interface Props {
   elementId: string;
 }
 
 const { elementId } = defineProps<Props>();
+const { y: scrollY } = useWindowScroll();
+
+const mounted = ref(false);
+
+const elementVisible = computedWithControl([mounted, scrollY], () => {
+  if (isClient && mounted.value) {
+    const element = document.getElementById(elementId);
+    if (element) {
+      const bounds = element?.getBoundingClientRect();
+      return Math.abs(bounds.top) < TOP_THRESHOLD;
+    }
+    return false;
+  }
+  return false;
+});
+
+onMounted(async () => {
+  mounted.value = true;
+});
 </script>
 
 <template>
   <a
     class="text-primary-500 hover:bg-silver-700/75 block cursor-pointer p-2 font-bold uppercase"
+    :class="{
+      'text-primary-300!': elementVisible,
+    }"
     @click="() => scrollToElement(elementId)"
   >
     <slot />
