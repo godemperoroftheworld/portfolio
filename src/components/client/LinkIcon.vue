f0dbbbffd3f9fb4fb6bc253f900b8e4b601807b1
Tenzin Pelletier <t2pellet@uwaterloo.ca>
2025-08-13 13:43:37 -0400

    fix: dark icons

--- src/components/client/LinkIcon.vue
+++ src/components/client/LinkIcon.vue
@@ -31,7 +31,7 @@ function onClick() {
     <div
       v-html="sanitizedIcon"
       :class="{ 'size-10': !large, 'size-16': large }"
-      class="group-[.card]/card:fill-silver-700 group-[.card]/card:text-silver-700 size-10 fill-black text-black dark:fill-white"
+      class="group-[.card]/card:fill-silver-700 group-[.card]/card:text-silver-700 group-[.card]/card:dark:fill-silver-100 group-[.card]/card:dark:text-silver-100 size-10 fill-black text-black dark:fill-white dark:text-white"
     />
     <span class="text-center text-sm">{{ label }}</span>
   </div>
