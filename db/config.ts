5c98605ed06d2ab18c7b846f2efa11412ad4cbc6
Tenzin Pelletier <t2pellet@uwaterloo.ca>
2026-06-13 19:59:10 -0400

    feat: comments display

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,19 @@
-import { defineDb } from 'astro:db';
+import { column, defineDb, defineTable } from 'astro:db';
+
+const Comment = defineTable({
+  columns: {
+    id:        column.number({ primaryKey: true }),
+    postSlug:  column.text(),
+    parentId:  column.number({ optional: true }),
+    name:      column.text(),
+    body:      column.text(),
+    likes:     column.number({ default: 0 }),
+    createdAt: column.date(),
+  }
+})
+
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: { Comment }
 });
--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,37 @@
-import { db } from 'astro:db';
+import { db, Comment } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+  await db.insert(Comment).values([
+    {
+      id: 1,
+      postSlug: 'breaking-the-script',
+      parentId: null,
+      name: 'Tenzin',
+      email: 'tenzin@example.com',
+      body: 'This is so cool!',
+      likes: 4,
+      createdAt: new Date('2025-06-09T10:32:00'),
+    },
+    {
+      id: 2,
+      postSlug: 'breaking-the-script',
+      parentId: null,
+      name: 'Jon',
+      email: 'jon@example.com',
+      body: 'Wowow. Bookmarked to come back to this.',
+      likes: 2,
+      createdAt: new Date('2025-06-10T14:15:00'),
+    },
+    {
+      id: 3,
+      postSlug: 'breaking-the-script',
+      parentId: 2,
+      name: 'Mat',
+      email: 'mat@example.com',
+      body: 'So true!',
+      likes: 3,
+      createdAt: new Date('2025-06-11T14:15:00'),
+    },
+  ])
 }
